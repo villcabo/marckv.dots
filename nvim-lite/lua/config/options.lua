420b3634f8ca22f2eafa077b8f9a84324c946e6a
Bismarck Villca <bismarck.villca@gmail.com>
2026-05-07 00:42:10 -0400

    revert(nvim-lite): remove OSC 52 SSH clipboard bridge
    Signed-off-by: Bismarck Villca <bismarck.villca@gmail.com>

--- nvim-lite/lua/config/options.lua
+++ nvim-lite/lua/config/options.lua
@@ -7,24 +7,3 @@ vim.g.autoformat = false
 -- Global statusline always at the bottom
 vim.o.laststatus = 3
 vim.o.cmdheight = 0
-
--- SSH clipboard bridge via OSC 52:
--- When editing remotely over SSH, copy lands in the LOCAL terminal's clipboard
--- (not the remote server's) so you can paste outside nvim on your own machine.
--- Requires Neovim >= 0.10 and an OSC 52-capable terminal (Kitty, WezTerm,
--- iTerm2, Alacritty with `enable_kitty_keyboard`, modern xterm, etc.)
-if os.getenv("SSH_TTY") or os.getenv("SSH_CONNECTION") then
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-    },
-  }
-  -- Make y/yy/d/p use the system clipboard by default (no need for "+y).
-  vim.opt.clipboard = "unnamedplus"
-end
